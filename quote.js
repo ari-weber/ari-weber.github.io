e8cb05be391c86c2c7d9b68887cefc20c45a8279
Ari Weber <arimweber@gmail.com>
2021-08-30 08:29:17 -0400

    Add files via upload

---
+++ quote.js
@@ -0,0 +1,16 @@
+let url = "https://quotes.rest/qod?language=en";
+const box = document.getElementById("quoteBox");
+
+fetch(url, {
+    method: "GET", // default is get
+    headers: {     // set headers easily
+      "Accept": "application/json",
+    },
+  }).then((res) => {
+    return res.json()
+  }).then((json) => {
+    console.log(json);
+    let text = "Quote of the Day: " + json.contents.quotes[0].quote;
+    console.log(text);
+    box.innerHTML = text;
+  });

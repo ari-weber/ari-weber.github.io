132f86446167784dd568edbe4720a4bdc28ebda0
Ari Weber <arimweber@gmail.com>
2021-08-31 18:44:45 -0400

    Add files via upload

--- universal_scripts.js
+++ universal_scripts.js
@@ -4,3 +4,12 @@ document.addEventListener('DOMContentLoaded', function(event){
         document.querySelector('body').style.opacity = 1;
     }, 1);
 })
+
+function myFunction() {
+    var x = document.getElementById("navDemo");
+    if (x.className.indexOf("w3-show") == -1) {
+      x.className += " w3-show";
+    } else { 
+      x.className = x.className.replace(" w3-show", "");
+    }
+  }

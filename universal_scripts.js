ff5851a115807fba5eb82c3c5e33826daf8d6dc7
Ari Weber <arimweber@gmail.com>
2021-08-31 08:42:22 -0400

    Add files via upload

--- universal_scripts.js
+++ universal_scripts.js
@@ -1,3 +1,6 @@
 document.addEventListener('DOMContentLoaded', function(event){
-    document.querySelector('body').style.opacity = 1;
+    document.querySelector('body').style.opacity = -10;
+    setTimeout(() => {
+        document.querySelector('body').style.opacity = 1;
+    }, 1);
 })

5c71c249d00b8966e6539f57a5bb1a98cf2d2d6f
Ari Weber <arimweber@gmail.com>
2021-09-10 14:54:47 -0400

    Add files via upload

--- quote.js
+++ quote.js
@@ -1,16 +1,32 @@
+gsap.registerPlugin(TextPlugin);
+
 let url = "https://quotes.rest/qod?language=en";
 const box = document.getElementById("quoteBox");
+const footer = document.getElementById("footer")
 
 fetch(url, {
     method: "GET", // default is get
     headers: {     // set headers easily
-      "Accept": "application/json",
-    },
-  }).then((res) => {
-    return res.json()
-  }).then((json) => {
-    console.log(json);
-    let text = "Quote of the Day: " + json.contents.quotes[0].quote + " - " + json.contents.quotes[0].author;
-    console.log(text);
-    box.innerHTML = text;
+    "Accept": "application/json",
+  },
+}).then((res) => {
+  return res.json()
+}).then((json) => {
+  console.log(json);
+  let text = "Quote of the Day: " + json.contents.quotes[0].quote + " - " + json.contents.quotes[0].author;
+  console.log(text);
+  box.innerText = "";
+  setTimeout(() => {
+    typeQuote(text);
+  }, 1000);
+});
+
+function typeQuote(quoteText){
+  let text =  String(quoteText);
+  gsap.to(box, {
+    duration: 1,
+    text: {
+      value: text
+    }
   });
+}
--- scripts.js
+++ scripts.js
@@ -1,1 +1,13 @@
-/**/
+var leftIcon = document.getElementById("leftIcon")
+var rightIcon = document.getElementById("rightIcon")
+
+document.addEventListener('DOMContentLoaded', function(event){
+    setTimeout(() => {
+        floatIn();
+    }, 500);
+})
+
+function floatIn(){
+    gsap.from("#leftIcon", {rotation: -90, x: -500, duration: 2});
+    gsap.from("#rightIcon", {rotation: 90, x: 500, duration: 2});
+}

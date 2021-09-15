c2b5d2d927f56d16f70d2e5f2dc0a12b842c687a
Ari Weber <arimweber@gmail.com>
2021-09-14 20:42:33 -0400

    Mobile-friendly update

--- scripts.js
+++ scripts.js
@@ -3,6 +3,8 @@ var leftIcon = document.getElementById("leftIcon");
 var rightIcon = document.getElementById("rightIcon");
 var grid = document.getElementById("grid");
 
+getWidthOfScreen();
+
 document.addEventListener('DOMContentLoaded', function(event){
     setTimeout(() => {
         floatIn();
@@ -12,4 +14,10 @@ document.addEventListener('DOMContentLoaded', function(event){
 function floatIn(){
     gsap.from("#leftIcon", {rotation: -90, x: -700, duration: 2});
     gsap.from("#rightIcon", {rotation: 90, x: 700, duration: 2});
+}
+
+function getWidthOfScreen(){
+    //if(true){
+    //    grid.className = "#gridSmallScreen";
+    //}
 }
--- universal_scripts.js
+++ universal_scripts.js
@@ -6,10 +6,10 @@ document.addEventListener('DOMContentLoaded', function(event){
 })
 
 function myFunction() {
-    var x = document.getElementById("navDemo");
-    if (x.className.indexOf("w3-show") == -1) {
-      x.className += " w3-show";
-    } else { 
-      x.className = x.className.replace(" w3-show", "");
-    }
+  var x = document.getElementById("navDemo");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  }else{
+    x.className = x.className.replace(" w3-show", "");
   }
+}
